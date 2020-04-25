129faa00b053d7927742995b727b12575be30ca3
박세은 <pkse1213@naver.com>
2020-04-25 14:28:45 +0900

    2020 카카오 모의

--- Main/main.cpp
+++ Main/main.cpp
@@ -4,17 +4,47 @@
 #include <queue>
 #include <deque>
 #include <vector>
+#include <math.h>
 #define MAX 1005
 #define INF 987654321
 using namespace std;
 typedef pair<int, int> pii;
 typedef long long ll;
 
-int S[MAX], DP[MAX];
+int M2017[7] = {0, 500, 300, 200, 50, 30, 10};
+
+vector<int> Score2017;
+vector<int> Score2018;
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
+    for(int i = 1; i <= 6; i++)
+    {
+        Score2017.insert(Score2017.end(), i, M2017[i]);
+    }
+    
+    int M = 1024;
+    double C = 0.5;
+    for(int i = 0; i < 5; i++)
+    {
+        M /= 2;
+        C *= 2;
+        Score2018.insert(Score2018.end(), C, M);
+    }
+    
+    int T, A, B;
+    cin >> T;
+    for(int i = 0; i < T; i++)
+    {
+        int Money = 0;
+        cin >> A >> B;
+        
+        if(0 < A && A <= 21) Money += Score2017[A-1];
+        if(0 < B && B <= 31) Money += Score2018[B-1];
+        printf("%d\n", Money*10000);
+    }
     return 0;
 }
